3c6ab49817be5e380bc53f19eab03ff59bf75abf
Will Braynen <wbraynen@gmail.com>
2021-12-29 18:01:48 -0700

    Added a mock weather service (to the test suite)

---
+++ MyLibrary/Tests/MyLibraryTests/MockWeatherService.swift
@@ -0,0 +1,28 @@
+import Alamofire
+import MyLibrary
+
+class MockWeatherService: WeatherService {
+    private var shouldSucceed: Bool
+    private var shouldReturnTemperatureWithAnEight: Bool
+
+    init(shouldSucceed: Bool, shouldReturnTemperatureWithAnEight: Bool) {
+        self.shouldSucceed = shouldSucceed
+        self.shouldReturnTemperatureWithAnEight = shouldReturnTemperatureWithAnEight
+    }
+
+    func getTemperature(completion: @escaping (_ response: Result<Int /* Temperature */, Error>) -> Void) {
+        switch (shouldSucceed, shouldReturnTemperatureWithAnEight) {
+        case (true, true):
+            let temperatureInFarenheit = 38
+            completion(.success(temperatureInFarenheit))
+
+        case (true, false):
+            let temperatureInFarenheit = 39
+            completion(.success(temperatureInFarenheit))
+
+        case (false, _):
+            let error404 = AFError.explicitlyCancelled
+            completion(.failure(error404))
+        }
+    }
+}
