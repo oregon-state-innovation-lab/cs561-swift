60b946ec7898d19d4207642db30a58ffcd6a3cdd
Ricardo Wu <stanfordcs91@gmail.com>
2022-01-10 15:07:22 -0800

    Update MyLibrary.swift

--- MyLibrary/Sources/MyLibrary/MyLibrary.swift
+++ MyLibrary/Sources/MyLibrary/MyLibrary.swift
@@ -2,7 +2,7 @@ public class MyLibrary {
     private let weatherService: WeatherService
 
     /// The class's initializer.
-    ///
+    ///  Ricardo's pull request
     /// Whenever we call the `MyLibrary()` constructor to instantiate a `MyLibrary` instance,
     /// the runtime then calls this initializer.  The constructor returns after the initializer returns.
     public init(weatherService: WeatherService? = nil) {
