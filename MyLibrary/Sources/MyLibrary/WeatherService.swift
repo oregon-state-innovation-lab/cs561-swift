d3800c0087a598bb3ecf5d8cb77aa587341c53ad
SrujanaJakkula <97068364+SrujanaJakkula@users.noreply.github.com>
2022-01-11 09:53:06 -0800

    Update WeatherService.swift

--- MyLibrary/Sources/MyLibrary/WeatherService.swift
+++ MyLibrary/Sources/MyLibrary/WeatherService.swift
@@ -4,10 +4,10 @@ public protocol WeatherService {
     func getTemperature(completion: @escaping (_ response: Result<Int /* Temperature */, Error>) -> Void)
 }
 
-class WeatherServiceImpl: WeatherService {
+public class WeatherServiceImpl: WeatherService {
     let url = "https://api.openweathermap.org/data/2.5/weather?q=corvallis&units=imperial&appid=<INSERT YOUR API KEY HERE>"
 
-    func getTemperature(completion: @escaping (_ response: Result<Int /* Temperature */, Error>) -> Void) {
+    public func getTemperature(completion: @escaping (_ response: Result<Int /* Temperature */, Error>) -> Void) {
         AF.request(url, method: .get).validate(statusCode: 200..<300).responseDecodable(of: Weather.self) { response in
             switch response.result {
             case let .success(weather):
