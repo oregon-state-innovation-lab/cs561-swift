cff91d9d5c72e66f1e532f08aef57f7a027f9ff5
Pranay240898 <97146583+Pranay240898@users.noreply.github.com>
2022-01-11 19:48:35 -0800

    Update MyLibrary.swift

--- MyLibrary/Sources/MyLibrary/MyLibrary.swift
+++ MyLibrary/Sources/MyLibrary/MyLibrary.swift
@@ -12,7 +12,7 @@ public class MyLibrary {
     public func isLucky(_ number: Int, completion: @escaping (Bool?) -> Void) {
         // Check the simple case first: 3, 5 and 8 are automatically lucky.
         if number == 3 || number == 5 || number == 8 {
-            completion(true)
+            completion(true)//lucky
             return
         }
 
