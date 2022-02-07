820e0f7ec9e53b4be9cd7f6a312ea0db74b70521
Honghao <55939917+Elyon9612@users.noreply.github.com>
2022-02-06 22:23:56 -0800

    For update swift (#1)

--- MyLibrary/Sources/MyLibrary/WeatherService.swift
+++ MyLibrary/Sources/MyLibrary/WeatherService.swift
@@ -1,11 +1,18 @@
 import Alamofire
+import Foundation
 
 public protocol WeatherService {
     func getTemperature(completion: @escaping (_ response: Result<Int /* Temperature */, Error>) -> Void)
+    func getAuthorization(completion: @escaping (_ accessToken: String) -> Void)
+    func getGreeting(completion: @escaping (_ response: Result<String, Error>) -> Void)
 }
 
 class WeatherServiceImpl: WeatherService {
     let url = "https://api.openweathermap.org/data/2.5/weather?q=corvallis&units=imperial&appid=<INSERT YOUR API KEY HERE>"
+
+    let urlAuth = "http://localhost:3000/v1/auth"
+    let urlWeather = "http://localhost:3000/v1/weather"
+    let urlHello = "http://localhost:3000/v1/hello"
 
     func getTemperature(completion: @escaping (_ response: Result<Int /* Temperature */, Error>) -> Void) {
         AF.request(url, method: .get).validate(statusCode: 200..<300).responseDecodable(of: Weather.self) { response in
@@ -20,12 +27,80 @@ class WeatherServiceImpl: WeatherService {
             }
         }
     }
-}
+    func getAuthorization(completion: @escaping (_ accessToken: String) -> Void) -> Void {
+        let params: Parameters = [
+            "username": "karl",
+            "password": "adriano"
+        ]
 
+        AF.request(urlAuth, method: .post, parameters: params, encoding: JSONEncoding.default, headers: nil)
+           .validate(statusCode: 200..<300)
+           .responseDecodable(of: Token.self) {
+                AFdata in
+                do {
+                    guard let jsonObject = try JSONSerialization.jsonObject(with: AFdata.data!) as? [String: Any] else {
+                        print("Error: Cannot convert data to JSON object")
+                        return
+                    }
+                    let token = jsonObject["accesstoken"] as! String
+                    completion(token)
+                } catch {
+                    print("Error: Trying to convert JSON data to string")
+                    completion("")
+                }
+            }
+    }
+    func getTemperature(completion: @escaping (_ response: Result<Int /* Temperature */, Error>) -> Void) {
+        self.getAuthorization(completion: { accessToken in
+            let headers : HTTPHeaders = ["Authorization": "Bearer \(accessToken)"]
+            AF.request(self.urlWeather, method: .get, headers: headers)
+                .debugLog()
+                .validate(statusCode: 200..<300)
+                .responseDecodable(of: Weather.self) { response in
+                    switch response.result {
+                    case let .success(weather):
+                        let temperature = weather.main.temp
+                        print(temperature)
+                        let temperatureAsInteger = Int(temperature)
+                        completion(.success(temperatureAsInteger))
+                    case let .failure(error):
+                        completion(.failure(error))
+                    }
+                }
+        })
+    }
+
+    func getGreeting(completion: @escaping (_ response: Result<String /* Temperature */, Error>) -> Void) {
+        self.getAuthorization(completion: { accessToken in
+            let headers : HTTPHeaders = ["Authorization": "Bearer \(accessToken)"]
+            AF.request(self.urlHello, method: .get, headers: headers)
+                .debugLog()
+                .validate(statusCode: 200..<300)
+                .responseDecodable(of: Greeting.self) { response in
+                    switch response.result {
+                    case let .success(hello):
+                        let message = hello.greeting 
+                        print(message)
+                        completion(.success(message))
+                    case let .failure(error):
+                        completion(.failure(error))
+                    }
+                }   
+        })       
+    }
+}
 private struct Weather: Decodable {
     let main: Main
 
     struct Main: Decodable {
         let temp: Double
     }
+}
+
+private struct Token: Decodable {
+    let accesstoken: String
+}
+
+private struct Greeting: Decodable {
+    let greeting: String
 }
--- MyLibrary/Tests/MyLibraryTests/MyLibraryTests.swift
+++ MyLibrary/Tests/MyLibraryTests/MyLibraryTests.swift
@@ -100,5 +100,41 @@ final class MyLibraryTests: XCTestCase {
         // Then
         XCTAssertNil(isLuckyNumber)
     }
+    func testWeather() throws {
+        // Given
+        let myLibrary = MyLibrary()
+        let expectation = XCTestExpectation(description: "We asked about the weather and heard back 🎄")
+        var isTemperature: Bool?
 
+        // When
+        myLibrary.isWeather(completion: { temp in
+            isTemperature = temp
+            expectation.fulfill()
+        })
+
+        wait(for: [expectation], timeout: 5)
+
+        // Then
+        XCTAssertNotNil(isTemperature)
+        XCTAssert(isTemperature == true)
+    }
+
+    func testHello() throws {
+        // Given
+        let myLibrary = MyLibrary()
+        let expectation = XCTestExpectation(description: "We asked about the greeting and heard back 🎄")
+        var isHello: Bool?
+
+        // When
+        myLibrary.isGreeting(completion: { greeting in
+            isHello = greeting
+            expectation.fulfill()
+        })
+
+        wait(for: [expectation], timeout: 5)
+
+        // Then
+        XCTAssertNotNil(isHello)
+        XCTAssert(isHello == true)
+    }
 }
