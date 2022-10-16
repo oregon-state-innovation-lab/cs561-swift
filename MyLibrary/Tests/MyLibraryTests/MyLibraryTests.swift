c76744b9a5d27123987e0bc3672f13ca2f08139b
JungCheChang <jungche0408@gmail.com>
2022-10-16 12:12:29 -0700

    Update MyLibraryTests.swift

--- MyLibrary/Tests/MyLibraryTests/MyLibraryTests.swift
+++ MyLibrary/Tests/MyLibraryTests/MyLibraryTests.swift
@@ -74,7 +74,7 @@ final class MyLibraryTests: XCTestCase {
         let jsonString = """
         {
             "main" : {
-                "temp" : 0.2
+                "temp" : 0.3
             }
         
         }
@@ -88,7 +88,7 @@ final class MyLibraryTests: XCTestCase {
         let weather = try decoder.decode(Weather.self, from: jsonData)
 
         // Then
-        XCTAssertEqual(weather.main.temp,0.2)
+        XCTAssertEqual(weather.main.temp,0.3)
     }
 
 }
