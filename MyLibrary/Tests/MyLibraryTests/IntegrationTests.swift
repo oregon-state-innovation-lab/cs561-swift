cfc88c5b508da2335090d556f5ae76d4732ea822
COE-LOAN-020\coe_user <madhuv@oregonstate.edu>
2022-10-16 19:50:46 -0700

    Added unit test cases and integration tests as a part of the module

---
+++ MyLibrary/Tests/MyLibraryTests/IntegrationTests.swift
@@ -0,0 +1,32 @@
+import XCTest
+@testable import MyLibrary
+final class IntegrationTests: XCTestCase {
+    func testWeatherService() async throws {
+        //Given
+        let my_service = WeatherServiceImpl()
+
+        //When
+        let temperatureCorvallis = try await my_service.getTemperature()
+
+
+        //Then
+        XCTAssertNotNil(temperatureCorvallis)
+        XCTAssert((temperatureCorvallis as Any)is Int)
+        
+
+    }
+
+    func testWeatherServiceMockData()  async throws{
+        let my_service = WeatherServiceImpl()
+
+        //When
+        let temperatureCorvallis = try await my_service.getTemperature()
+
+
+        //Then
+        XCTAssertNotNil(temperatureCorvallis)
+        XCTAssertEqual(temperatureCorvallis,62)
+        
+    }
+          
+}
--- MyLibrary/Tests/MyLibraryTests/MyLibraryTests.swift
+++ MyLibrary/Tests/MyLibraryTests/MyLibraryTests.swift
@@ -1,6 +1,5 @@
 import XCTest
-import MyLibrary
-
+@testable import MyLibrary
 final class MyLibraryTests: XCTestCase {
     func testIsLuckyBecauseWeAlreadyHaveLuckyNumber() async {
         // Given
@@ -69,4 +68,37 @@ final class MyLibraryTests: XCTestCase {
         XCTAssertNil(isLuckyNumber)
     }
 
+    func testIsLuckyWithJson() async {
+        // Given
+        let mockWeatherService = MockWeatherService(
+            shouldSucceed: true,
+            shouldReturnTemperatureWithAnEight: false
+        )
+
+        let myLibrary = MyLibrary(weatherService: mockWeatherService)
+
+        // When
+        let isLuckyNumber = await myLibrary.isLucky(8)
+
+        // Then
+        XCTAssertNotNil(isLuckyNumber)
+        XCTAssert(isLuckyNumber == true)
+    }
+    func testFinalpath() async throws {
+        //given
+        let filePath = try XCTUnwrap(Bundle.module.path(forResource:"data",ofType:"json"))
+        let jsonString = try String(contentsOfFile: filePath)
+        let jsonData = Data(jsonString.utf8)
+        let jsonDecoder = JSONDecoder()
+
+        //when
+       let result = try jsonDecoder.decode(Weather.self, from: jsonData)
+
+        //then
+        XCTAssertNotNil(filePath)
+        XCTAssertNotNil(result)
+        XCTAssertNotNil(result.main.temp)
+        XCTAssert(result.main.temp == 293.62)
+
+    }
 }
