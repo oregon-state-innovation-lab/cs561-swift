45d609353ce68040d2f63ccc4f8880ffc6807d50
Saikiran Uppu <uppus@oregonstate.edu>
2022-10-16 20:30:28 -0700

    unitandintegrationtest

--- MyLibrary/Tests/MyLibraryTests/MyLibraryTests.swift
+++ MyLibrary/Tests/MyLibraryTests/MyLibraryTests.swift
@@ -1,7 +1,56 @@
 import XCTest
 import MyLibrary
+@testable import MyLibrary
 
+final class IntergrationTest: XCTestCase
+{
+
+func test1() async throws{
+    let weatherserv = WeatherServiceImpl()
+    let temperature = try await weatherserv.getTemperature()
+    print(temperature)
+    XCTAssertEqual(temperature,61)
+}
+func test2() async throws{
+    let weatherserv = WeatherServiceImpl()
+    let temperature = try await weatherserv.getTemperature()
+    print(temperature)
+    XCTAssert((temperature as Any) is Int)
+}
+}
 final class MyLibraryTests: XCTestCase {
+    
+
+func testNotNil() throws {
+        //given
+        let filePath = try XCTUnwrap(Bundle.module.path(forResource:"data",ofType:"json"))
+        let jsonString = try String(contentsOfFile: filePath)
+        let jsonData = Data(jsonString.utf8)
+        let jsonDecoder = JSONDecoder()
+
+        // When
+        let weather = try jsonDecoder.decode(Weather.self, from: jsonData)
+        print(weather.main.temp)
+
+        //Then
+        XCTAssertNotNil(weather.main.temp)
+    }
+
+        func testFilePath() throws {
+        //given
+        let filePath = try XCTUnwrap(Bundle.module.path(forResource:"data",ofType:"json"))
+        let jsonString = try String(contentsOfFile: filePath)
+        let jsonData = Data(jsonString.utf8)
+        let jsonDecoder = JSONDecoder()
+
+        // When
+        let weather = try jsonDecoder.decode(Weather.self, from: jsonData)
+        print(weather.main.temp)
+
+        //Then
+        XCTAssertNotNil(filePath)
+    }
+
     func testIsLuckyBecauseWeAlreadyHaveLuckyNumber() async {
         // Given
         let mockWeatherService = MockWeatherService(
@@ -36,7 +85,7 @@ final class MyLibraryTests: XCTestCase {
         XCTAssert(isLuckyNumber == true)
     }
 
-    func testIsNotLucky() async {
+   func testIsNotLucky() async {
         // Given
         let mockWeatherService = MockWeatherService(
             shouldSucceed: true,
@@ -70,3 +119,4 @@ final class MyLibraryTests: XCTestCase {
     }
 
 }
+
