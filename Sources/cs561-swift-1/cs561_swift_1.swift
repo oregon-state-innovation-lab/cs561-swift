ae5d3c9cb57ce4f644b6190c8af6de95f56e7b39
JungCheChang <jungche0408@gmail.com>
2022-10-23 19:02:57 -0700

    init

---
+++ Sources/cs561-swift-1/cs561_swift_1.swift
@@ -0,0 +1,6 @@
+public struct cs561_swift_1 {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/cs561-swift-1Tests/cs561_swift_1Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import cs561_swift_1
+
+final class cs561_swift_1Tests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(cs561_swift_1().text, "Hello, World!")
+    }
+}
