539cca5ef046dfefe536b01aaa9d647be35d03a4
chiluksr <104956850+chiluksr@users.noreply.github.com>
2022-10-23 18:51:59 -0700

    Updated the test file

--- MyLibrary/Tests/MyLibraryTests/MyLibraryTests.swift
+++ MyLibrary/Tests/MyLibraryTests/MyLibraryTests.swift
@@ -1,27 +1,120 @@
-import Alamofire
+import XCTest
 import MyLibrary
+@testable import MyLibrary
 
-class MockWeatherService: WeatherService {
-    private var shouldSucceed: Bool
-    private var shouldReturnTemperatureWithAnEight: Bool
+final class MyLibraryTests: XCTestCase {
 
-    init(shouldSucceed: Bool, shouldReturnTemperatureWithAnEight: Bool) {
-        self.shouldSucceed = shouldSucceed
-        self.shouldReturnTemperatureWithAnEight = shouldReturnTemperatureWithAnEight
-    }
-
-    /// Returns current temperature in Farenheight
-    func getTemperature() async throws -> Int {
-        switch (shouldSucceed, shouldReturnTemperatureWithAnEight) {
-        case (true, true):
-            return 38
-
-        case (true, false):
-            return 39
-
-        case (false, _):
-            let error404 = AFError.explicitlyCancelled
-            throw error404
-        }
-    }
+    func test1() async throws{
+    let weatherserv = WeatherServiceImpl()
+    let temperature = try await weatherserv.getTemperature()
+    print(temperature)
+    XCTAssertEqual(temperature,62)
 }
+func test2() async throws{
+    let weatherserv = WeatherServiceImpl()
+    let temperature = try await weatherserv.getTemperature()
+    print(temperature)
+    XCTAssert((temperature as Any) is Int)
+}
+
+    
+
+    func testNotNil() throws {
+        //given
+        let filePath = try XCTUnwrap(Bundle.module.path(forResource:"data",ofType:"json"))
+        let jsonString = try String(contentsOfFile: filePath)
+        let jsonData = Data(jsonString.utf8)
+        let jsonDecoder = JSONDecoder()
+
+        // When
+        let weather = try jsonDecoder.decode(Weather.self, from: jsonData)
+        print(weather.main.temp)
+
+        //Then
+        XCTAssertNotNil(weather.main.temp)
+    }
+
+        func testFilePath() throws {
+        //given
+        let filePath = try XCTUnwrap(Bundle.module.path(forResource:"data",ofType:"json"))
+        let jsonString = try String(contentsOfFile: filePath)
+        let jsonData = Data(jsonString.utf8)
+        let jsonDecoder = JSONDecoder()
+
+        // When
+        let weather = try jsonDecoder.decode(Weather.self, from: jsonData)
+        print(weather.main.temp)
+
+        //Then
+        XCTAssertNotNil(filePath)
+    }
+
+    func testIsLuckyBecauseWeAlreadyHaveLuckyNumber() async {
+        // Given
+        let mockWeatherService = MockWeatherService(
+            shouldSucceed: true,
+            shouldReturnTemperatureWithAnEight: false
+        )
+
+        let myLibrary = MyLibrary(weatherService: mockWeatherService)
+
+        // When
+        let isLuckyNumber = await myLibrary.isLucky(8)
+
+        // Then
+        XCTAssertNotNil(isLuckyNumber)
+        XCTAssert(isLuckyNumber == true)
+    }
+
+    func testIsLuckyBecauseWeatherHasAnEight() async throws {
+        // Given
+        let mockWeatherService = MockWeatherService(
+            shouldSucceed: true,
+            shouldReturnTemperatureWithAnEight: true
+        )
+
+        let myLibrary = MyLibrary(weatherService: mockWeatherService)
+
+        // When
+        let isLuckyNumber = await myLibrary.isLucky(0)
+
+        // Then
+        XCTAssertNotNil(isLuckyNumber)
+        XCTAssert(isLuckyNumber == true)
+    }
+
+    func testIsNotLucky() async {
+        // Given
+        let mockWeatherService = MockWeatherService(
+            shouldSucceed: true,
+            shouldReturnTemperatureWithAnEight: false
+        )
+
+        let myLibrary = MyLibrary(weatherService: mockWeatherService)
+
+        // When
+        let isLuckyNumber = await myLibrary.isLucky(7)
+
+        // Then
+        XCTAssertNotNil(isLuckyNumber)
+        XCTAssert(isLuckyNumber == false)
+    }
+
+    func testIsNotLuckyBecauseServiceCallFails() async {
+        // Given
+        let mockWeatherService = MockWeatherService(
+            shouldSucceed: false,
+            shouldReturnTemperatureWithAnEight: false
+        )
+
+        let myLibrary = MyLibrary(weatherService: mockWeatherService)
+
+        // When
+        let isLuckyNumber = await myLibrary.isLucky(7)
+
+        // Then
+        XCTAssertNil(isLuckyNumber)
+    }
+
+}
+
