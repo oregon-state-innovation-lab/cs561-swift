1d9fd9fdba00df71ef5c033b3a66dda385a129c7
Tanmay Badageri <tanmayb47@gmail.com>
2022-10-25 19:18:13 -0700

    rectified commit #1

--- MyLibrary/Sources/MyLibrary/WeatherService.swift
+++ MyLibrary/Sources/MyLibrary/WeatherService.swift
@@ -5,8 +5,12 @@ public protocol WeatherService {
 }
 
 class WeatherServiceImpl: WeatherService {
-    let url = "https://api.openweathermap.org/data/2.5/weather?q=corvallis&units=imperial&appid=<INSERT YOUR API KEY HERE>"
-
+    enum BaseUrl :String {
+           case production = "https://api.openweathermap.org/data/2.5/weather"
+           case mockServer = "http://localhost:3000/data/2.5/weather"
+       }
+       
+       let url = "https://cs561.herokuapp.com/"
     func getTemperature() async throws -> Int {
         return try await withCheckedThrowingContinuation { continuation in
             AF.request(url, method: .get).validate(statusCode: 200..<300).responseDecodable(of: Weather.self) { response in
---
+++ MyLibrary/Tests/MyLibraryTests/tan.swift
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+import XCTest
+import MyLibrary
+@testable import MyLibrary
+
+
+final class MyIntegrationTest: XCTestCase {
+    
+    func testingWeatherModel() async throws {
+        
+        let weatherService = WeatherServiceImpl()
+        
+        
+        let temperature = try await weatherService.getTemperature()
+        
+        XCTAssertEqual(temperature, 62)
+    }
+}
+
+
+
